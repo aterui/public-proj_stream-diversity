ef468c13a81290ed2ae0aa409e31299b85e12885
AkiraTerui <hanabi0111@gmail.com>
2020-07-10 10:50:06 -0400

    Edits: data-format01_mn.R

--- data-format01_mn.R
+++ data-format01_mn.R
@@ -1,10 +1,29 @@
+#' ---
+#' title: MN data formatting
+#' author: Akira Terui
+#' output:
+#'   html_document:
+#'     theme: paper
+#'     toc: true
+#'     toc_float: true
+#' ---
+
+#' # Session infomation
   
+  sessionInfo()
+
+#' # Load library
+
   # Load library
   rm(list=ls(all.names=T))
   library(tidyverse)
 
+#' # Species removal information
+#' This information is included for comparison betwee Terui's and Kim's script
+  
   # formatting information --------------------------------------------------
   ## species name used in Kim's original script for removal
+  ## this code section is created to just compare Terui's and Kim's scripts
   sp_rm <- c("AMEIURUS_SPP.","CAMPOSTOMA_SPP.","CARPIODES_SPP.","CATOSTOMIDAE_-_UNIDENTIFIED",
              "COMMON_SHINER_X_CREEK_CHUB","COMMON_SHINER_X_EMERALD_SHINER","COMMON_SHINER_X_HORNYHEAD",
              "COMMON_SHINER_X_SOUTHERN_REDBELLY_DACE","COMMON_SHINER_X_STONEROLLER","COMMON_SHINER_X_UNKNOWN",
@@ -32,10 +51,22 @@
   #MN.FishData[MN.FishData=="TROUTPERCH"]<- "TROUT_PERCH"
 
   
+
+#' # Read data
+#' **NOTE: use `readr::read_csv()` instead of `read.csv()` to read data - `read.csv()` treated blanks weiredly in this dataset**
+#' 
+#' - `01_data/org-data_mn_visit.csv` - visit data
+#' - `01_data/org-data_mn_station.csv` - site date
+#' - `01_data/org-data_mn_fish.csv` - fish data
+#+ message = FALSE, warning = FALSE
+  
   # Read data ---------------------------------------------------------------
   dat_visit <- read_csv("01_data/org-data_mn_visit.csv") %>% mutate(FieldNum = str_to_upper(FieldNum) )
   dat_site <- read_csv("01_data/org-data_mn_station.csv") %>% mutate(FieldNum = str_to_upper(FieldNum) )
   dat_fish <- read_csv("01_data/org-data_mn_fish.csv") %>% mutate(FieldNum = str_to_upper(FieldNum) )
+
+#' # Fish data formatting
+#' Remove unidentified or hybrid species. These are identified if `Name2` is either `NA`, `hybrid` or `larvae`
 
   # Fish data formatting ----------------------------------------------------
   ## Check unidentified or hybrid species
@@ -43,29 +74,47 @@
   unique(dat_fish$CommonName[dat_fish$Name2 %in% c("hybrid", "larvae")])
 
   ## format CommonName
-  ## drop unidentified or hybrid species
+  ## replace space with underscore
+  ## rename to upppercases
+  ## remove '_(AMMOCOETE)' from string
+  ## replace 'NORTHERN_HOG_SUCKER' with 'NORTHERN_HOGSUCKER'
+  ## replace 'TROUT-PERCH' and 'TROUTPERCH' with 'TROUT_PERCH'
+  ## drop unidentified
+  ## drop hybrid and larval species
+  ## remove GOLDFISH and PEARL DACE
+  
   dat_fish %>%
     mutate(CommonName = str_replace(CommonName, pattern = " ", replacement = "_") ) %>%
     mutate(CommonName = str_to_upper(CommonName) ) %>%
+    mutate(CommonName = str_replace(CommonName, "_\\(AMMOCOETE\\)", "")) %>% # remove '_(AMMOCOETE)' from string
+    mutate(CommonName = str_replace(CommonName, "NORTHERN_HOG_SUCKER", "NORTHERN_HOGSUCKER")) %>%
+    mutate(CommonName = str_replace(CommonName, "TROUT-PERCH", "TROUT_PERCH")) %>%
+    mutate(CommonName = str_replace(CommonName, "TROUTPERCH", "TROUT_PERCH")) %>%
     drop_na(Name2) %>% # remove NA
     filter(!(Name2 %in% c("hybrid", "larvae")) ) %>% # remove c("hybrid", "larvae")
     filter(!(CommonName %in% c("GOLDFISH", "PEARL_DACE")) ) -> dat_fish # remove c("GOLDFISH", "PEARL_DACE")
   
-  ## difference from Kim's script - why pearl dace removed?
+  ## check difference from Kim's script - why pearl dace removed?
   ## one observation fewer than Kim's original file
   print(unique(dat_fish$CommonName[dat_fish$CommonName %in% sp_rm]) )
-  
-  
+
+#' # Merge datasets
+#' Merge `dat_fish`, `dat_visit` and `dat_site`
+#' 
+#' - `dat_fish` as a base dataset because it includes fish data
+#' - `dat_visit` is joined to obtain sampling date
+#' - `dat_site` is joined to obtain GPS location (Lat and Lon)
+
   # Add visit and station data to fish data ---------------------------------
-  dat_fish %>%
+  dat = dat_fish %>%
     left_join(dat_visit, by = "VisitNum") %>% # merge Visit data
     rename(FieldNum = FieldNum.x) %>% # rename FieldNum (derive from fish_dat)
     left_join(dat_site, by = "FieldNum") %>% # merge site information
     drop_na(FieldNum.y) %>% # drop NA FieldNum in dat_visit
     select(c(VisitNum, FieldNum, Name1, Name2, CommonName,
              VisitDate, StationLength, Number, LAT8xDD, LON8xDD) ) %>% # select columns
-    rename(Genus = Name1, Species = Name2, Count = Number, Lat = LAT8xDD, Lon = LON8xDD) -> dat_fish # rename columns
+    rename(Genus = Name1, Species = Name2, Count = Number, Lat = LAT8xDD, Lon = LON8xDD) # rename columns
   
-  length(unique(dat_fish$VisitNum) ) # n = 3049
-  length(unique(dat_fish$FieldNum) ) # n = 2372
+  length(unique(dat$VisitNum) ) # n = 3049
+  length(unique(dat$FieldNum) ) # n = 2372
                 
