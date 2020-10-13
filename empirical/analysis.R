c9e271d30473b40d0ea9c9437c2ad52eb1a82b1d
AkiraTerui <hanabi0111@gmail.com>
2020-10-12 20:56:44 -0400

    Update analysis.R

--- empirical/analysis.R
+++ empirical/analysis.R
@@ -11,20 +11,36 @@
 
   dat_hkd <- read_csv("data_out/data_hkd.csv") %>% mutate(region = "hokkaido")
   dat_mw <- read_csv("data_out/data_mw.csv") %>% mutate(region = "midwest")
-  dat <- bind_rows(dat_hkd, dat_mw)
+  dat <- bind_rows(dat_hkd, dat_mw) %>% 
+    rename(gamma = Estimator,
+           gamma_lower = '95% Lower',
+           gamma_upper = '95% Upper') %>% 
+    mutate(beta = gamma - mu_alpha,
+           beta_lower = gamma_lower - mu_alpha,
+           beta_upper = gamma_upper - mu_alpha)
   
-  fit <- lm(log(Estimator) ~ log(area)*region + p_branch*region + p_branch*log(area) +
-                             I(p_urban + p_agri) + scale(mu_temp) + scale(mu_prec),
-                             dat)
-  models <- dredge(fit)  
+  fit <- lm(log(gamma) ~ log(area)*region + p_branch*region + p_branch*log(area) +
+                         I(p_urban + p_agri) + scale(mu_temp) + scale(mu_prec), dat)
   
+  fit_upper <- lm(log(gamma_upper) ~ log(area)*region + p_branch*region + p_branch*log(area) +
+                                     I(p_urban + p_agri) + scale(mu_temp) + scale(mu_prec), dat)
+  
+  fit_lower <- lm(log(gamma_lower) ~ log(area)*region + p_branch*region + p_branch*log(area) +
+                                     I(p_urban + p_agri) + scale(mu_temp) + scale(mu_prec), dat)
+  
+  m <- dredge(fit)
+  re <- model.avg(object = m, subset = delta < 2)
+  summary(re)
   
   ggplot(dat) +
-    geom_point(aes(x = p_branch, y = log(Estimator), color = region))
+    geom_point(aes(x = p_branch, y = log(gamma), color = region))
   
   ggplot(dat) +
-    geom_point(aes(x = log(area), y = log(Estimator), color = region))
+    geom_point(aes(x = log(area), y = log(gamma), color = region))
   
   ggplot(dat) +
-    geom_point(aes(x = p_branch, y = log(Estimator - mu_alpha), color = region))
+    geom_point(aes(x = p_branch, y = log(beta), color = region))
+
+  ggplot(dat) +
+    geom_point(aes(x = log(area), y = log(beta), color = region))
   
