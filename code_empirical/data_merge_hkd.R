a3b0891cf95b7961fcaf64fb59999e8736aa35d5
aterui <hanabi0111@gmail.com>
2021-08-31 00:51:49 -0400

    edits: deta_merge

--- code_empirical/data_merge_hkd.R
+++ code_empirical/data_merge_hkd.R
@@ -3,61 +3,65 @@
   
   rm(list = ls(all.names = TRUE))
   setwd(here::here("code_empirical"))
-  pacman::p_load(tidyverse, sf, iNEXT, foreach, magrittr)  
+  pacman::p_load(tidyverse, 
+                 sf, 
+                 iNEXT, 
+                 foreach, 
+                 magrittr)  
   
 # read gis data -----------------------------------------------------------
 
-  watershed <- st_read(dsn = 'data_gis/albers_watershed_hkd_final.gpkg') %>% 
-    rename(watershed_id = id) %>% 
-    arrange(watershed_id)
+  albers_watershed <- st_read(dsn = 'data_gis/albers_watershed_hkd_final.gpkg') %>% 
+    dplyr::rename(watershed_id = wsd_id) %>% 
+    dplyr::arrange(watershed_id)
   
 # merge fish and gis data -------------------------------------------------
 
   d0 <- read_csv('data_org_hkd/data_fmt_hkd_latest.csv') %>% 
     st_as_sf(coords = c("Lon", "Lat"), crs = 4326) %>% 
-    st_transform(crs = st_crs(watershed)$wkt) %>% 
-    filter(Year >= 1990) %>% 
-    st_join(watershed)
+    st_transform(crs = st_crs(albers_watershed)$wkt) %>% 
+    dplyr::filter(Year >= 1990) %>% 
+    st_join(albers_watershed)
   
   n_site_stat <- d0 %>%
-    as_tibble() %>% 
-    group_by(watershed_id) %>% 
-    summarise(n_site = n_distinct(SiteID)) %>% 
-    right_join(as_tibble(watershed), by = "watershed_id") %>% 
-    select(watershed_id, n_site, area, n_branch) %>% 
-    mutate(density_site = n_site/area)
+    dplyr::as_tibble() %>% 
+    dplyr::group_by(watershed_id) %>% 
+    dplyr::summarise(n_site = n_distinct(SiteID)) %>% 
+    dplyr::right_join(as_tibble(albers_watershed), by = "watershed_id") %>% 
+    dplyr::select(watershed_id, n_site, area, n_branch) %>% 
+    dplyr::mutate(density_site = n_site/area)
   
   ## watershed ID with more than X site
   ## watershed ID with more than X branch
   wsd_candidate <- n_site_stat %>% 
-    filter(n_site >= 10 & n_branch >= 10) %>% 
-    pull(watershed_id)
+    dplyr::filter(n_site >= 10 & n_branch >= 10) %>% 
+    dplyr::pull(watershed_id)
   
   dat_fish <- d0 %>% 
-    filter(watershed_id %in% wsd_candidate) %>% 
-    as_tibble()
+    dplyr::filter(watershed_id %in% wsd_candidate) %>% 
+    dplyr::as_tibble()
 
   dat_alpha <- dat_fish %>% 
-    group_by(watershed_id, SiteID) %>% 
-    summarise(alpha_div = n_distinct(Species)) %>% 
-    group_by(watershed_id) %>% 
-    summarise(mu_alpha = mean(alpha_div))
+    dplyr::group_by(watershed_id, SiteID) %>% 
+    dplyr::summarise(alpha_div = n_distinct(Species)) %>% 
+    dplyr::group_by(watershed_id) %>% 
+    dplyr::summarise(mu_alpha = mean(alpha_div))
 
 # iNEXT -------------------------------------------------------------------
 
   ## convert data to frequency
   dat_freq <- dat_fish %>% 
-    group_by(watershed_id, Species) %>% 
-    summarise(freq = n_distinct(SiteID)) %>% 
-    pivot_wider(id_cols = watershed_id,
-                names_from = Species,
-                values_from = freq,
-                values_fill = list(freq = 0)) 
+    dplyr::group_by(watershed_id, Species) %>% 
+    dplyr::summarise(freq = n_distinct(SiteID)) %>% 
+    tidyr::pivot_wider(id_cols = watershed_id,
+                       names_from = Species,
+                       values_from = freq,
+                       values_fill = list(freq = 0)) 
   
   dat_freq <- dat_fish %>% 
-    group_by(watershed_id) %>% 
-    summarise(n_site = n_distinct(SiteID)) %>% 
-    left_join(dat_freq, by = "watershed_id")
+    dplyr::group_by(watershed_id) %>% 
+    dplyr::summarise(n_site = n_distinct(SiteID)) %>% 
+    dplyr::left_join(dat_freq, by = "watershed_id")
   
   list_freq <- foreach(i = seq_len(nrow(dat_freq))) %do% {
     x <- as.vector(sort(unlist(dat_freq[i,-which(colnames(dat_freq) == 'watershed_id')]),
@@ -69,7 +73,8 @@
   all(dat_freq$n_site == unlist(lapply(list_freq, function(x)x[1]))) # check if the first element of the list is n_site
   
   ## iNEXT: select watershed with 90% coverage
-  full_est <- iNEXT(list_freq, datatype = "incidence_freq")
+  full_est <- iNEXT(list_freq,
+                    datatype = "incidence_freq")
   
   wsd_subset <- full_est[[1]] %>% 
     filter(SC > 0.9) %>% 
@@ -83,7 +88,7 @@
   
   ## export diversity estimates
   dat_hkd <- div_est %>%
-    left_join(as_tibble(watershed), by = "watershed_id") %>% 
+    left_join(as_tibble(albers_watershed), by = "watershed_id") %>% 
     left_join(dat_alpha, by = "watershed_id") %>% 
     select(-geom)
   
@@ -104,7 +109,7 @@
   write_csv(splist, "data_out/data_hkd_splist.csv")
   
   ## export watershed polygon
-  albers_wsd_subset <- watershed %>% 
+  albers_wsd_subset <- albers_watershed %>% 
     filter(watershed_id %in% wsd_subset)
   
   st_write(albers_wsd_subset,
@@ -113,11 +118,17 @@
   
   ## export point data
   albers_point <- d0 %>% 
-    filter(watershed_id %in% wsd_subset) %>% 
-    select(SiteID, geometry) %>% 
-    distinct()
-  
+    distinct(SiteID, geometry)
+
   st_write(albers_point,
+           "data_gis/albers_point_hkd.gpkg",
+           append = FALSE)
+        
+  albers_point_subset <- d0 %>% 
+    filter(watershed_id %in% wsd_subset) %>% 
+    distinct(SiteID, geometry)
+  
+  st_write(albers_point_subset,
            "data_gis/albers_point_subset_hkd.gpkg",
            append = FALSE)
   
