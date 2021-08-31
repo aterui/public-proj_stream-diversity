b4ce39afe71a0f839de580bb3859769273502f8a
aterui <hanabi0111@gmail.com>
2021-08-31 17:24:08 -0400

    Update figure_diversity.R

--- code_empirical/figure_diversity.R
+++ code_empirical/figure_diversity.R
@@ -2,7 +2,9 @@
 # setup -------------------------------------------------------------------
 
   rm(list = ls(all.names = TRUE))
-  pacman::p_load(tidyverse, sf, patchwork)
+  pacman::p_load(tidyverse, 
+                 sf, 
+                 patchwork)
   setwd(here::here("code_empirical"))
 
 # set plot theme ----------------------------------------------------------
@@ -44,7 +46,7 @@
   
   dat <- bind_rows(dat_hkd, dat_mw) %>% 
     rename(gamma = Estimator, alpha = mu_alpha) %>% 
-    mutate(beta = gamma/alpha) %>% 
+    mutate(beta = gamma / alpha) %>% 
     pivot_longer(cols = c(alpha, beta, gamma),
                  names_to = "metric")
   
@@ -62,35 +64,62 @@
   ## area prediction
   dat_area <- dat %>% 
     group_by(region) %>% 
-    summarise(area = seq(min(area), max(area), length = 100)) %>% 
-    left_join(dat_base, by = "region") %>% 
+    summarise(area = seq(min(area), 
+                         max(area),
+                         length = 100)) %>% 
+    left_join(dat_base, 
+              by = "region") %>% 
     mutate(p_branch = f_p_branch)
   
   dat_bp <- dat %>% 
     group_by(region) %>% 
-    summarise(p_branch = seq(min(p_branch), max(p_branch), length = 100)) %>% 
-    left_join(dat_base, by = "region") %>% 
+    summarise(p_branch = seq(min(p_branch), 
+                             max(p_branch), 
+                             length = 100)) %>% 
+    left_join(dat_base, 
+              by = "region") %>% 
     mutate(area = f_area)
   
   ### area
-  dat_area$alpha <- 10^predict(fit$alpha$model, newdata = dat_area)
-  dat_area$beta <- 10^predict(fit$beta$model, dat_area)
-  dat_area$gamma <- 10^predict(fit$gamma$model, dat_area)
+  dat_area$alpha <- 10^predict(fit$alpha$model, 
+                               newdata = dat_area)
+  dat_area$beta <- 10^predict(fit$beta$model, 
+                              dat_area)
+  dat_area$gamma <- 10^predict(fit$gamma$model,
+                               dat_area)
   dat_area <- dat_area %>%  
-    pivot_longer(cols = c(alpha, beta, gamma), names_to = "metric")
+    pivot_longer(cols = c(alpha, beta, gamma), 
+                 names_to = "metric")
   
   ### p_branch
-  dat_bp$alpha <- 10^predict(fit$alpha$model, dat_bp)
-  dat_bp$beta <- 10^predict(fit$beta$model, dat_bp)
-  dat_bp$gamma <- 10^predict(fit$gamma$model, dat_bp)
+  dat_bp$alpha <- 10^predict(fit$alpha$model, 
+                             dat_bp)
+  dat_bp$beta <- 10^predict(fit$beta$model,
+                            dat_bp)
+  dat_bp$gamma <- 10^predict(fit$gamma$model, 
+                             dat_bp)
   dat_bp <- dat_bp %>%  
-    pivot_longer(cols = c(alpha, beta, gamma), names_to = "metric")
+    pivot_longer(cols = c(alpha, beta, gamma),
+                 names_to = "metric")
   
 # fig ---------------------------------------------------------------------
   
   region_label <- c("Hokkaido", "Midwest")
   names(region_label) <- c("hokkaido", "midwest")
   
+  # line type would be "dashed" if 95% CI overlaps zero
+  line_type <- lapply(fit,
+                      FUN = function(x) {
+                        y <- confint.default(x$model)
+                        id <- which(rownames(y) %in% c("log(area, 10)",
+                                                       "log(p_branch, 10)"))
+                        rowSums(y[id, ] > 0) == 1
+                      }) %>% 
+    dplyr::bind_cols() %>% 
+    sapply(FUN = function(x) ifelse(x == TRUE, "dashed", "solid"))
+  
+  
+  # size scaling
   g1 <- ggplot(dat, aes(x = area,
                         y = value,
                         color = metric)) +
@@ -100,17 +129,21 @@
     geom_point(alpha = 0.25) +
     geom_line(data = dat_area,
               aes(x = area,
-                  y = value)) +
+                  y = value,
+                  linetype = metric)) +
     scale_color_hue(name = NULL,
                     labels = c(expression(alpha~"diversity"),
                                expression(beta~"diversity"),
                                expression(gamma~"diversity"))) +
+    scale_linetype_manual(values = line_type[1,],
+                          guide = "none") +
     xlab(expression("Watershed area ("~km^2~")")) +
     scale_y_continuous(trans='log10') +
     scale_x_continuous(trans='log10') +
     ylab("Species richness") +
     theme(legend.position = "none")
   
+  # complexity scaling
   g2 <- ggplot(dat, aes(x = p_branch,
                         y = value,
                         color = metric)) +
@@ -126,7 +159,7 @@
                     labels = c(expression(alpha~"diversity"),
                                expression(beta~"diversity"),
                                expression(gamma~"diversity"))) +
-    scale_linetype_manual(values = c("dashed", "solid", "solid"),
+    scale_linetype_manual(values = line_type[2,],
                           guide = "none") +
     xlab("Branching probability") +
     scale_y_continuous(trans='log10') +
