f210a8d78c64340f547f383210065e5c5905fb12
aterui <hanabi0111@gmail.com>
2021-08-31 17:26:06 -0400

    Update figure_correlation.R
    quite logit transformation

--- code_empirical/figure_correlation.R
+++ code_empirical/figure_correlation.R
@@ -15,16 +15,13 @@
     rename(gamma = Estimator,
            alpha = mu_alpha) %>% 
     mutate(beta = gamma/alpha,
-           logit_forest = log(frac_forest) - log(1 - frac_forest),
-           logit_urban = log(frac_urban) - log(1 - frac_urban),
-           logit_agri = log(frac_agri) - log(1 - frac_agri),
            dam_density = n_dam/area) %>% 
     mutate(resid_temp = resid(lm(mean_temp ~ region, data = .)),
            resid_ppt = resid(lm(mean_ppt ~ region, data = .)),
            resid_dem = resid(lm(mean_dem ~ region, data = .)),
-           resid_forest = resid(lm(logit_forest ~ region, data = .)),
-           resid_urban = resid(lm(logit_urban ~ region, data = .)),
-           resid_agri = resid(lm(logit_agri ~ region, data = .)),
+           resid_forest = resid(lm(frac_forest ~ region, data = .)),
+           resid_urban = resid(lm(frac_urban ~ region, data = .)),
+           resid_agri = resid(lm(frac_agri ~ region, data = .)),
            resid_dam = resid(lm(dam_density ~ region, data = .)))  
   
   M <- dat %>% 
@@ -33,9 +30,9 @@
            'Temperature' = resid_temp,
            'Precipitation' = resid_ppt,
            'Elevation' = resid_dem,
-           'Logit fraction of forest' = resid_forest,
-           'Logit fraction of urban' = resid_urban,
-           'Logit fraction of agriculture' = resid_agri,
+           'Fraction of forest' = resid_forest,
+           'Fraction of urban' = resid_urban,
+           'Fraction of agriculture' = resid_agri,
            'Dam density' = resid_dam) %>% 
     cor()
   
